075d356fddf6672dedc462487dc6ef0a38ed52f5
Victor Li <victor.li158@gmail.com>
2017-05-16 13:34:27 -0700

    Add SessionReducer

--- frontend/nearbytes.jsx
+++ frontend/nearbytes.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import Root from './components/root';
 // import configureStore from './store/store';
-import * as SessionApiUtil from './util/session_api_util';
+// import * as SessionApiUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    window.login = SessionApiUtil.login;
-    window.logout = SessionApiUtil.logout;
-    window.signup = SessionApiUtil.signup;
+    // window.login = SessionApiUtil.login;
+    // window.logout = SessionApiUtil.logout;
+    // window.signup = SessionApiUtil.signup;
     ReactDOM.render(<h1>Welcome to NearBytes</h1>, root);
 });

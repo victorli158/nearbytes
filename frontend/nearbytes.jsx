43fcbc6ef2a7789f6ef989281edb2a4aa6e1f201
Victor Li <victor.li158@gmail.com>
2017-05-16 13:59:51 -0700

    Add store

--- frontend/nearbytes.jsx
+++ frontend/nearbytes.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import Root from './components/root';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 // import * as SessionApiUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
-    // window.login = SessionApiUtil.login;
-    // window.logout = SessionApiUtil.logout;
-    // window.signup = SessionApiUtil.signup;
-    ReactDOM.render(<h1>Welcome to NearBytes</h1>, root);
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  const root = document.getElementById('root');
+  // window.login = SessionApiUtil.login;
+  // window.logout = SessionApiUtil.logout;
+  // window.signup = SessionApiUtil.signup;
+  ReactDOM.render(<h1>Welcome to NearBytes</h1>, root);
 });

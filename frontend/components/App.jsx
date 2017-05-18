1380090ba3e8c45a36b29936324a47b601a54c66
Victor Li <victor.li158@gmail.com>
2017-05-17 21:01:01 -0700

    Add tinted home image to home page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,6 +5,7 @@ import { Route, Link, Switch, Redirect } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import NavBarContainer from './nav_bar/nav_bar_container';
 import { Footer } from './footer.jsx';
+import { Home } from './home/home';
 
 const App = () => (
   <div className="container">
@@ -12,6 +13,7 @@ const App = () => (
       <NavBarContainer />
     </header>
     <Switch>
+      <AuthRoute exact path="/" component={Home}/>
       <AuthRoute path="/login" component={SessionFormContainer}/>
       <AuthRoute path="/signup" component={SessionFormContainer}/>
     </Switch>
---
+++ frontend/components/home/home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Home = () => (
+    <div className="home">
+      <div className="tint"></div>
+    </div>
+);
